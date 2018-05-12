1ec0e4911959ea28abb10e355868de293e01976c
bluesky7517 <bluesky7517@hotmail.com>
2018-05-12 23:05:47 +0800

    Procfile

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+
+from django.urls import urls
+from django.contrib import admin
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    urls('admin/', admin.site.urls),
 ]
